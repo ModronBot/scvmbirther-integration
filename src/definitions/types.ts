eece8cad7349d7a5c86eac4989b775998034f1e0
Dario Sechi <loller790@gmail.com>
2021-02-23 17:46:35 +0100

    Add Character type definition with example

---
+++ src/definitions/types.ts
@@ -0,0 +1,61 @@
+export type Character = {
+  ability: {
+    agility: number
+    presence: number
+    strength: number
+    toughness: number
+  }
+  actions: string[]
+  class: string
+  equipment: {
+    armor: string
+    shield: boolean
+    weapon: string
+  }
+  habits: string
+  health: number
+  money: number
+  name: string
+  inventory: string[]
+  omens: {
+    notation: string
+    total: number
+  }
+  scrolls: string[]
+  tales: string
+  traits: string
+}
+
+/*
+
+  EXAMPLE:
+
+  character = {
+      ability: {
+          agility: 0,
+          presence: 0,
+          strength: 1,
+          toughness: 0
+      },
+      actions: ['Hamfund the Squire', 'Poltroon the Court Jester'],
+      class: 'Wretched Royalty',
+      equipment: {
+          armor: 'Fur armor',
+          shield: true,
+          weapon: 'Femur'
+      },
+      habits: 'Prone to substance abuse and lazy.',
+      health: 3,
+      money: 168,
+      name: 'Börda',
+      inventory: ['Waterskin', 'Femur', 'Fur armor', 'Sling', 'Backpack', 'Silver crucifix', 'Tent'],
+      omens: {
+          notation: 'd2',
+          total: 2
+      },
+      scrolls: ['Whispers Pass the Gate'],
+      tales: 'Your earliest memories are of following an army in eastern Wästland.',
+      traits: 'You have thirty or so friends who never let you down: YOUR TEETH.'
+  }
+
+*/
